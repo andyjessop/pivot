3bc87e7847f0a21617962079861a913dc6baa957
Andy Jessop <andrewdjessop@gmail.com>
2023-03-26 15:40:08 +0200

    Add services-context

--- apps/client/src/services/index.ts
+++ apps/client/src/services/index.ts
@@ -1,46 +1,6 @@
-import { createContext, useContext, useEffect, useState } from 'react';
-import { cacheService } from '../modules/cache.module';
-import { routerService } from '../modules/router.module';
-import { ExtractInstance } from '@pivot/injectable';
+import { servicesContext } from '@pivot/services-context';
+import { config } from './config';
 
-const config = {
-  cache: cacheService,
-  router: routerService,
-};
+const { Services, useService } = servicesContext(config);
 
-export const Services = createContext(config);
-
-export function useService<K extends keyof typeof config>(key: K) {
-  const context = useContext(Services);
-
-  // The context contains injectables, so we need to extract the instance.
-  type Service = ExtractInstance<(typeof config)[K]>;
-
-  const [service, setService] = useState<Service | null>(null);
-
-  if (context === undefined) {
-    throw new Error('useService must be used within a ServicesProvider');
-  }
-
-  useEffect(() => {
-    resolveService();
-
-    return () => {
-      if (service) {
-        context[key].onDestroy(service as any);
-      }
-    };
-  }, []);
-
-  return service;
-
-  async function resolveService() {
-    try {
-      const response = await context[key].get();
-
-      setService(response as Service);
-    } catch (error) {
-      /* ... */
-    }
-  }
-}
+export { Services, useService };
---
+++ packages/services-context/src/index.test.ts
@@ -0,0 +1,5 @@
+describe('services-context', () => {
+  it('should work', () => {
+    expect(true).toEqual(true);
+  });
+});
