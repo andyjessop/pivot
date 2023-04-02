55c5e01c3ea9afc09b05bb5f67da7094658649a8
Andy Jessop <andrewdjessop@gmail.com>
2023-04-02 19:39:00 +0200

    Commit

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -9,7 +9,7 @@ import { App } from './App';
 main();
 
 async function main() {
-  // If we're in development, start the mock server. This starts a ServiceWorker
+  // If we're in development or CI, start the mock server. This starts a ServiceWorker
   // which intercepts fetch requests and returns mocks according to the contents
   // of ./shared/mocks/handlers. See https://mswjs.io/ for more details.
   const { server } = await import('./server/worker');
