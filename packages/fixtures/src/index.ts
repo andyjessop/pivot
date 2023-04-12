a7677ba47ed98ed881b72e87d4d1efb7b8ce0e4e
Andy Jessop <andrewdjessop@gmail.com>
2023-04-12 21:59:07 +0200

    Add team fixtures

--- packages/fixtures/src/index.ts
+++ packages/fixtures/src/index.ts
@@ -1,3 +1,7 @@
-export async function getFixture(projectId: string, component: string) {
-  return await import(`./${projectId}`).then((mod) => mod[component]);
+export async function getProjectFixture(projectId: string, component: string) {
+  return import(`./project_${projectId}`).then((mod) => mod[component]);
+}
+
+export async function getUserFixture(userId: string, component: string) {
+  return import(`./user_${userId}`).then((mod) => mod[component]);
 }
