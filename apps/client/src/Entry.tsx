02dc4b94b31e48cbdf96f7a92cd6b2c895b6db69
Andy Jessop <andrewdjessop@gmail.com>
2023-04-13 18:11:20 +0200

    Fix paths

--- apps/client/src/Entry.tsx
+++ apps/client/src/Entry.tsx
@@ -1,13 +1,13 @@
 import { Suspense } from 'react';
 
-import { selectAuth } from '@pivot/client/auth';
 import { MainNav, Navbar, UserNav } from '@pivot/client/components';
-import { selectNavItems } from '@pivot/client/nav';
-import { selectRouteName } from '@pivot/client/router';
 import { horizontalLeftContent as layout } from '@pivot/design/css';
 import { cx } from '@pivot/util/classname';
 
 import { useSelector, useService } from '~app';
+import { selectAuth } from '~app/modules/auth';
+import { selectNavItems } from '~app/modules/nav';
+import { selectRouteName } from '~app/modules/router';
 import logo from '~assets/react.svg';
 
 import styles from './app.module.css';
