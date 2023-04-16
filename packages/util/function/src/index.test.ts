f3df6420f1d3b8b6ed86e2d6fd968f6303fe8e13
Andy Jessop <andrewdjessop@gmail.com>
2023-04-16 11:18:22 +0200

    Add tests

--- packages/util/function/src/index.test.ts
+++ packages/util/function/src/index.test.ts
@@ -1,27 +1,33 @@
-import { externallyResolvablePromise } from './index';
+import { retry } from '.';
 
-describe('util-promise', () => {
-  describe('externallyResolvablePromise', () => {
-    it('should resolve with external method', async () => {
-      const result = 'foobar';
-      const { promise, resolve } = externallyResolvablePromise();
+describe('retry function', () => {
+  test('succeeds without retries', async () => {
+    const fn = () => Promise.resolve(42);
+    const result = await retry(fn);
+    expect(result).toBe(42);
+  });
 
-      resolve(result);
+  test('succeeds with retries', async () => {
+    let counter = 0;
+    const fn = () => {
+      counter++;
+      if (counter === 2) {
+        return Promise.resolve(42);
+      }
+      return Promise.reject(new Error('Failed'));
+    };
+    const result = await retry(fn);
+    expect(result).toBe(42);
+  });
 
-      return promise.then((res) => {
-        expect(res).toEqual(result);
-      });
-    });
+  test('fails when retries are exhausted', async () => {
+    const fn = () => Promise.reject(new Error('Failed'));
+    await expect(retry(fn)).rejects.toThrow('Retry failed');
+  });
 
-    it('should reject with external method', async () => {
-      const result = 'bazbar';
-      const { promise, reject } = externallyResolvablePromise();
-
-      reject(result);
-
-      return promise.catch((res) => {
-        expect(res).toEqual(result);
-      });
-    });
+  test('respects custom successCondition', async () => {
+    const fn = () => Promise.resolve(42);
+    const successCondition = (result: number) => result === 24;
+    await expect(retry(fn, successCondition)).rejects.toThrow('Retry failed');
   });
 });
