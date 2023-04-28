e4790380e60283fb319dbc6ad7ee99c5d2a2de6b
Andy Jessop <andrewdjessop@gmail.com>
2023-04-28 23:21:37 +0200

    Add design

--- packages/fixtures/src/fixtures/features.ts
+++
@@ -1,8 +0,0 @@
-export const features = [
-  {
-    created_at: '2023-02-05T18:25:33.107325+00:00',
-    name: 'NEW_FEATURE',
-    uuid: 'cf490256-f32b-48ec-ba32-7d16557799c0',
-    project_id: 'f4c727f9-6331-439e-b34d-8e056a2aa282',
-  },
-];
--- packages/fixtures/src/fixtures/variables.ts
+++
@@ -1,8 +0,0 @@
-export const variables = [
-  {
-    created_at: '2023-02-05T18:00:30.507299+00:00',
-    name: 'API_URL',
-    uuid: '4e698d9f-3715-4dcc-b9a8-cd1c47b9ed3a',
-    project_id: 'f4c727f9-6331-439e-b34d-8e056a2aa282',
-  },
-];
