a7fa26f7b762ec26daccd637c43352819db258cb
Andy Jessop <andrewdjessop@gmail.com>
2023-05-04 22:54:19 +0200

    Fix lint errors

--- packages/client/pending-deployment/src/types/actions.ts
+++ packages/client/pending-deployment/src/types/actions.ts
@@ -1,10 +1,10 @@
 import { DeploymentFeature } from '@pivot/client/deployments';
 
-import { PendingDeployment } from './state';
+import { Model } from './model';
 
 export interface Actions {
-  set(deployment: PendingDeployment | null): void;
-  update(deployment: Partial<PendingDeployment>): void;
+  set(deployment: Model | null): void;
+  update(deployment: Partial<Model>): void;
   updateFeature(uuid: string, feature: Partial<DeploymentFeature>): void;
   updateVariable(uuid: string, value: string): void;
 }
